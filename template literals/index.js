3590be0152b909dc0c8178e3eb6c5370d9209747
Nathan Sanford <nwsanfor24@gmail.com>
2020-02-19 20:29:03 -0500

    add more shit

--- template literals/index.js
+++ template literals/index.js
@@ -1,13 +1,32 @@
 const music = {
-    // code here
+    artist: "Tool",
+    song: "Stinkfist",
+    album: "Aenema"
 };
 
 // write code between the <div> tags to output the data from the music object above
 const songSnippet = `
     <div class="song">
-  
+  We are ${music.artist}!
+  This is our song ${music.song} from our album ${music.album}!
     </div>
   `;
 
 const element = document.getElementById("music");
 element.innerHTML = songSnippet;
+
+const scottPilgrim = {
+    song: "Sex Bo-bomb",
+    think: "death and stuff",
+    count: "1, 2, 3, 4"
+};
+
+const scottSnippet = `
+    <div class="song">
+    We are ${scottPilgrim.song} and we're here to make you think about 
+    ${scottPilgrim.think}! ${scottPilgrim.count}!!!
+    </div>
+`
+
+const newElement = document.getElementById("scott");
+newElement.innerHTML = scottSnippet; 
