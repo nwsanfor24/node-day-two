317fb3cbb0b1fd05c32e44d3324ef0aae9c50ac5
Nathan Sanford <nwsanfor24@gmail.com>
2020-02-19 21:31:05 -0500

    end of class

--- destructuring/index.js
+++ destructuring/index.js
@@ -8,6 +8,7 @@ const arya = {
 
 // CODE BELOW HERE
 
+const { first, last, origin, allegiance } = arya;
 
 
 console.log(first); // <= Arya
@@ -32,7 +33,7 @@ const john = {
 };
 
 // CODE BELOW HERE
-
+const { brother1, brother2 } = john.family.brothers;
 
 console.log(brother1); // <= Rob Stark
 console.log(brother2); // <= Rickon Stark
@@ -42,7 +43,7 @@ console.log(brother2); // <= Rickon Stark
 const characters = ["Ned Stark", "The Quiet Wolf", "House Stark"];
 
 // CODE BELOW HERE
-
+const [name, alias, allegiance] = characters;
 
 console.log(name, alias, allegiance);
 
@@ -51,6 +52,6 @@ console.log(name, alias, allegiance);
 const skills = 'The Usurper, male, Baratheon, Cersei';
 
 // CODE BELOW HERE
-
+const [alias, gender, family, spouse] = skills.split(",");
 
 console.log(alias, gender, family, spouse); // print them by variable name
